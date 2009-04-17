b6ae242bc54032d4202c6cd1e745de59c21a312c
Cyan Ogilvie <cyan@tirion.(none)>
2009-04-17 12:55:46 +0200

    Updates

---
+++ js/m2/m2.js
@@ -0,0 +1,515 @@
+// vim: ft=javascript foldmethod=marker foldmarker=<<<,>>> ts=4 shiftwidth=4
+
+if (typeof window.console == 'undefined') {
+	window.console = {
+		log: function(msg) {dump(msg+'\n');},
+		warn: function(msg) {alert(arguments);},
+		error: function(msg) {alert(arguments);}
+	};
+}
+
+function m2_connect(host, port) { //<<<
+	console.log('attempting to connect to m2_node on ('+host+') ('+port+')');
+	this.unique_id = 0;
+	var handlers = new Hash();
+	var event_handlers = new Hash();
+
+	var pending = new Hash();
+	var ack_pend = new Hash();
+	var jm = new Hash();
+	var jm_prev_seq = new Hash();
+	var socket = jsSocket({
+		keepalive: null,
+		logger: console.log,
+		debug: true
+	});
+
+	// msg_handler <<<
+	var msg_handler = {
+		gotMsg: function(reqseq, type, payload) {
+			//console.log('gotMsg: reqseq: ('+reqseq+'), type: ('+type+'), payload: ('+payload+')');
+			if (handlers.hasItem(reqseq)) {
+				handlers.getItem(reqseq)(type, payload);
+				if (type == "ack" || type == "nack") {
+					//console.log('removing handler for ('+reqseq+')');
+					handlers.removeItem(reqseq);
+				}
+			} else {
+				//console.log('no handlers found for seq: '+reqseq);
+			}
+		},
+
+		closed: function(data) {
+			//console.log('server closed connection');
+			if (console !== undefined) {
+				console.log('server closed connection');
+			}
+		}
+	};
+	// msg_handler >>>
+
+	// api <<<
+	var api = {
+		_svcs: new Hash(),
+
+		_set_default_msg_fields: function(msg) { //<<<
+			if (msg.svc === undefined)		{msg.svc		= 'sys';}
+			if (msg.type === undefined)		{msg.type		= 'req';}
+			if (msg.seq === undefined)		{msg.seq		= '';}
+			if (msg.prev_seq === undefined)	{msg.prev_seq	= '0';}
+			if (msg.meta === undefined)		{msg.meta		= {};}
+			if (msg.oob_type === undefined)	{msg.oob_type	= '1';}
+			if (msg.oob_data === undefined)	{msg.oob_data	= '1';}
+		},
+
+		//>>>
+		_receive_msg: function(msg_raw) { //<<<
+			var lineend, pre_raw, pre, fmt, hdr_len, data_len, hdr, data, ofs, msg;
+			//console.log('received complete message: ('+msg_raw+')');
+
+			lineend = msg_raw.indexOf("\n");
+			if (lineend == -1) {
+				throw 'corrupt m2 message header: '+msg_raw;
+			}
+			pre_raw = msg_raw.substr(0, lineend);
+			pre = parse_tcl_list(pre_raw);
+			fmt = pre[0];
+			if (fmt != 1) {
+				throw 'Cannot parse m2 message serialization format: ('+fmt+')'
+			}
+			hdr_len = Number(pre[1]);
+			data_len = Number(pre[2]);
+			hdr = parse_tcl_list(msg_raw.substr(lineend + 1, hdr_len));
+			ofs = lineend + 1 + hdr_len;
+			//console.log('hdr: ',hdr);
+			data = msg_raw.substr(ofs, data_len);
+			//console.log('ofs: '+ofs+', data_len: '+data_len+', data: '+data);
+			//console.log('msg_raw.substr('+ofs+'): ('+msg_raw.substr(ofs)+')');
+			msg = {
+				'svc':		hdr[0],
+				'type':		hdr[1],
+				'seq':		hdr[2],
+				'prev_seq':	hdr[3],
+				'meta':		hdr[4],
+				'oob_type':	hdr[5],
+				'oob_data':	hdr[6],
+				'data':		data
+			};
+			this._got_msg(msg);
+		},
+
+		//>>>
+		_svc_avail: function(new_svcs) { //<<<
+			var i;
+
+			for (i=0; i<new_svcs.length; i++) {
+				this._svcs.setItem(new_svcs[i], true);
+			}
+
+			dispatch_event('svc_avail_changed', []);
+		},
+
+		//>>>
+		svc_avail: function(svc) { //<<<
+			return this._svcs.hasItem(svc);
+		},
+
+		//>>>
+		_svc_revoke: function(revoked_svcs) { //<<<
+			var i;
+
+			for (i=0; i<revoked_svcs.length; i++) {
+				if (this._svcs.hasItem(revoked_svcs[i])) {
+					this._svcs.removeItem(revoked_svcs[i]);
+				}
+			}
+
+			dispatch_event('svc_avail_changed', []);
+		},
+
+		//>>>
+		_jm_can: function(msg) { //<<<
+			var prev_seq_arr, i, tmp, prev_seq, cb;
+
+			if (jm_prev_seq.hasItem(msg.seq)) {
+				jm_prev_seq.removeItem(msg.seq);
+			}
+
+			prev_seq_arr = parse_tcl_list(msg.prev_seq);
+
+			for (i=0; i<prev_seq_arr.length; i++) {
+				prev_seq = prev_seq_arr[i];
+				tmp = jm.getItem(prev_seq);
+				tmp--;
+				jm.setItem(prev_seq, tmp);
+				if (pending.hasItem(prev_seq)) {
+					cb = pending.getItem(prev_seq);
+					if (cb !== '') {
+						try {
+							cb(msg, prev_seq);
+						} catch(e) {
+							if (console !== undefined) {
+								console.log('error calling callback for jm_can: '+e);
+							}
+						}
+					}
+				}
+				if (jm.getItem(prev_seq) <= 0) {
+					if (pending.hasItem(prev_seq)) {
+						pending.removeItem(prev_seq);
+					}
+					if (jm.hasItem(prev_seq)) {
+						jm.removeItem(prev_seq);
+					}
+				}
+			}
+		},
+
+		//>>>
+		_response: function(msg) { //<<<
+			var prev_seq_arr, i, prev_seq, cb, msgcopy;
+
+			msgcopy = msg;
+			prev_seq_arr = parse_tcl_list(msg.prev_seq);
+			for (i=0; i<prev_seq_arr.length; i++) {
+				prev_seq = prev_seq_arr[i];
+				msgcopy.prev_seq = prev_seq;
+				if (pending.hasItem(prev_seq)) {
+					cb = pending.getItem(prev_seq);
+					if (cb !== '') {
+						try {
+							cb(msgcopy);
+						} catch (e) {
+							if (console !== undefined) {
+								console.log('error calling callback for response type: '+msg.type+': '+e);
+							}
+						}
+					}
+				}
+				if (!jm.hasItem(prev_seq)) {
+					continue;
+				}
+				if (
+					jm.getItem(prev_seq) <= 0 &&
+					!ack_pend.hasItem(prev_seq)
+				) {
+					if (pending.hasItem(prev_seq)) {
+						pending.removeItem(prev_seq);
+					}
+					if (jm.hasItem(prev_seq)) {
+						jm.removeItem(prev_seq);
+					}
+				}
+			}
+		},
+
+		//>>>
+		_got_msg: function(msg) { //<<<
+			console.log('got m2 msg:');
+			console.log('msg.svc: ('+msg.svc+')');
+			console.log('msg.type: ('+msg.type+')');
+			console.log('msg.seq: ('+msg.seq+')');
+			console.log('msg.prev_seq: ('+msg.prev_seq+')');
+			console.log('msg.meta: ('+msg.meta+')');
+			console.log('msg.oob_type: ('+msg.oob_type+')');
+			console.log('msg.oob_data: ('+msg.oob_data+')');
+			console.log('msg.data: ('+msg.data+')');
+			switch (msg.type) {
+				case 'svc_avail':
+					this._svc_avail(parse_tcl_list(msg.data));
+					break;
+
+				case 'svc_revoke':
+					this._svc_revoke(parse_tcl_list(msg.data));
+					break;
+
+				case 'jm_can':
+					this._jm_can(msg);
+					break;
+
+				case 'pr_jm':
+					jm.setItem(msg.prev_seq, jm.getItem(msg.prev_seq) + 1);
+				case 'ack':
+				case 'nack':
+				case 'jm':
+					this._response(msg);
+					break;
+
+				default:
+					break;
+			}
+		},
+
+		//>>>
+		_serialize_msg: function(msg) { //<<<
+			var hdr, sdata;
+
+			hdr = serialize_tcl_list([
+				msg.svc,
+				msg.type,
+				msg.seq,
+				msg.prev_seq,
+				msg.meta,
+				msg.oob_type,
+				msg.oob_data
+			]);
+			sdata = serialize_tcl_list([
+				'1',
+				hdr.length,
+				msg.data.length
+			]);
+			sdata += '\n' + hdr + msg.data;
+
+			return sdata;
+		},
+
+		//>>>
+		send: function(msg) { //<<<
+			var sdata;
+
+			this._set_default_msg_fields(msg);
+			sdata = this._serialize_msg(msg);
+			queue.enqueue(sdata, msg);
+		}
+
+		//>>>
+	};
+	// api >>>
+	this.api = api;
+
+	// queue manager <<<
+	var queue = {
+		_defrag_buf: new Hash,
+		_msgid_seq: 0,
+		_queues: new Hash,
+
+		_receive_raw: function(packet) { //<<<
+			//console.log('_queue_receive_raw: ('+packet+')');
+			var lineend, head, msgid, is_tail, fragment_len, frag, rest;
+			rest = packet;
+			while (rest.length > 0) {
+				lineend = rest.indexOf("\n");
+				if (lineend == -1) {
+					console.log('corrupt fragment header: '+rest);
+				}
+				head = parse_tcl_list(rest.substr(0, lineend));
+				msgid = Number(head[0]);
+				is_tail = Boolean(head[1]);
+				fragment_len = Number(head[2]);
+				frag = rest.substr(lineend + 1, fragment_len);
+				rest = rest.substr(lineend + 1 + fragment_len);
+				this._receive_fragment(msgid, is_tail, frag);
+			}
+		},
+
+		//>>>
+		_receive_fragment: function(msgid, is_tail, frag) { //<<<
+			var complete, so_far;
+			//console.log('_receive_fragment: msgid: ('+msgid+'), is_tail: ('+is_tail+'), frag: ('+frag+')');
+			if (this._defrag_buf.hasItem(msgid)) {
+				so_far = this._defrag_buf.getItem(msgid);
+			} else {
+				so_far = '';
+			}
+			so_far += frag;
+			if (is_tail) {
+				complete = so_far
+				this._defrag_buf.removeItem(msgid);
+				api._receive_msg(complete);
+			} else {
+				this._defrag_buf.setItem(msgid, so_far);
+			}
+		},
+
+		//>>>
+		enqueue: function(sdata, msg) { //<<<
+			var msgid, payload, dgram;
+			/* ----- Queueing requires that we get writable notifications -----
+			var target, msgid, target_queue;
+
+			target = this.assign(msg);
+			msgid = this._msgid_seq++;
+			if (!this._queues.hasItem(target)) {
+				target_queue = [];
+			} else {
+				target_queue = this._queues.getItem(target);
+			}
+			target_queue.push([msgid, sdata]);
+			this._queues.setItem(target, target_queue);
+			*/
+
+			console.log('sending msg:');
+			console.log('msg.svc: ('+msg.svc+')');
+			console.log('msg.type: ('+msg.type+')');
+			console.log('msg.seq: ('+msg.seq+')');
+			console.log('msg.prev_seq: ('+msg.prev_seq+')');
+			console.log('msg.meta: ('+msg.meta+')');
+			console.log('msg.oob_type: ('+msg.oob_type+')');
+			console.log('msg.oob_data: ('+msg.oob_data+')');
+			console.log('msg.data: ('+msg.data+')');
+
+			msgid = this._msgid_seq++;
+
+			payload = String(msgid)+' 1 '+sdata.length+'\n'+sdata;
+			//console.log('writing dgram: ('+dgram+')');
+			socket.send(payload);
+		}
+
+		//>>>
+	}
+
+	// queue manager >>>
+
+	socket.onData = function(data) {
+		queue._receive_raw(data);
+	};
+
+	socket.onStatus = function(type, val) {
+		switch (type) {
+			case 'connecting':
+				break;
+			case 'connected':
+				api.send({
+					type: 'neighbour_info',
+					data: serialize_tcl_list(['type', 'application'])
+				});
+
+				try {
+					dispatch_event('connected', true);
+				} catch (e) {
+					console.log('dispatching connected event went badly: '+e);
+				}
+				break;
+			case 'disconnected':
+				msg_handler.closed();
+				try {
+					dispatch_event('connected', false);
+				} catch (e) {
+					console.log('dispatching disconnected event went badly: '+e);
+				}
+				break;
+			case 'waiting':
+				break;
+			case 'failed':
+				break;
+			default:
+				console.log('Unhandled socket status: "'+type+'"');
+				break;
+		}
+	}
+
+	socket.onLoaded = function(data) {
+		socket.open(host, port);
+	};
+
+	this.req = function(svc, data, cb) { //<<<
+		var seq, msg;
+
+		seq = this.unique_id++;
+
+		api.send({
+			svc:	svc,
+			type:	'req',
+			seq:	seq,
+			data:	data
+		});
+
+		pending.setItem(seq, cb);
+		ack_pend.setItem(seq, 1);
+		jm.setItem(seq, 0);
+
+		return seq;
+	};
+
+	//>>>
+	this.rsj_req = function(jm_seq, data, cb) { //<<<
+		var seq, msg;
+
+		seq = this.unique_id++;
+
+		api.send({
+			svc:		'',
+			type:		'rsj_req',
+			seq:		seq,
+			prev_seq:	jm_seq,
+			data:		data
+		});
+
+		pending.setItem(seq, cb);
+		ack_pend.setItem(seq, 1);
+		jm.setItem(seq, 0);
+
+		return seq;
+	};
+
+	//>>>
+	this.jm_disconnect = function(jm_seq, prev_seq) { //<<<
+		var msg;
+
+		api.send({
+			svc:		'',
+			type:		'jm_disconnect',
+			seq:		jm_seq,
+			prev_seq:	prev_seq
+		});
+
+		if (jm_prev_seq.hasItem(jm_seq)) {
+			var idx = jm_prev_seq.getItem(jm_seq).indexOf(prev_seq);
+			var new_prev_seqs, old_prev_seqs, i;
+
+			if (idx == -1) {
+				// prev_seq invalid
+			} else {
+				jm.setItem(prev_seq, jm.getItem(prev_seq) - 1);
+				if (jm.getItem(prev_seq) <= 0) {
+					pending.removeItem(prev_seq);
+					jm.removeItem(prev_seq);
+				}
+				new_prev_seqs = [];
+				old_prev_seqs = jm_prev_seq.getItem(jm_seq);
+				for (i=0; i<old_prev_seqs.length; i++) {
+					if (old_prev_seqs[i] != prev_seq) {
+						new_prev_seqs.push(old_prev_seqs[i]);
+					}
+				}
+				if (new_prev_seqs.length > 0) {
+					jm_prev_seq.setItem(jm_seq, new_prev_seqs);
+				} else {
+					jm_prev_seq.removeItem(jm_seq);
+				}
+			}
+		} else {
+			// Can't find jm_prev_seq(jm_seq)
+		}
+	};
+
+	//>>>
+
+	this.listen_event = function(event, cb) { //<<<
+		var existing;
+		if (event_handlers.hasItem(event)) {
+			existing = event_handlers.getItem(event);
+		} else {
+			existing = [];
+		}
+		existing.push(cb);
+		event_handlers.setItem(event, existing);
+	};
+
+	//>>>
+	this.dispatch_event = function(event, params) { //<<<
+		if (event_handlers.hasItem(event)) {
+			var cbs = event_handlers.getItem(event);
+			for (i=0; i<cbs.length; i++) {
+				//console.log('calling cb '+cbs[i]+',('+params+')');
+				cbs[i](params);
+			}
+		}
+	};
+
+	//>>>
+
+	return this;
+}
+
+//>>>
---
+++ js/m2/tcllist.js
@@ -0,0 +1,491 @@
+// vim: ft=javascript foldmethod=marker foldmarker=<<<,>>> ts=4 shiftwidth=4
+
+function is_space(char) { //<<<
+	// Not perfect
+	if (
+		char == ' ' ||
+		char == '\t' ||
+		char == '\n' ||
+		char == '\r' ||
+		char == '\v'
+	) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//>>>
+function unicode_char(value) { //<<<
+	return String.fromCharCode(value);
+	/*
+	var hexstr;
+	var rhexmap = [
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+	];
+	hexstr = '\\u';
+	hexstr += rhexmap[(value / (1<<12)) & 0xF];
+	hexstr += rhexmap[(value / (1<<8)) & 0xF];
+	hexstr += rhexmap[(value / (1<<4)) & 0xF];
+	hexstr += rhexmap[value & 0xF];
+	return eval('"'+hexstr+'"');
+	*/
+}
+
+//>>>
+function char_is_hex(char) { //<<<
+	switch (char) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+			return true;
+
+		default:
+			return false;
+	}
+}
+
+//>>>
+function parse_tcl_list(str) { //<<<
+	var ofs = -1;
+	var parts = new Array();
+	var elem = '';
+	var in_elem = false;
+	var braced = false;
+	var quoted = false;
+	var bracedepth = 0;
+	var braceescape = false;
+	var elemstart = false;
+	var escaped = false;
+	var escape_seq = '';
+	var escape_mode = '';
+	var needspace = false;
+	var braceofs = 0;
+	var quoteofs = 0;
+	var c;
+	var finished = false;
+	var cont = false;
+	var acc;
+	var pow;
+
+	var hexmap = {
+		'a': 10,
+		'b': 11,
+		'c': 12,
+		'd': 13,
+		'e': 14,
+		'f': 15,
+		'A': 10,
+		'B': 11,
+		'C': 12,
+		'D': 13,
+		'E': 14,
+		'F': 15
+	};
+
+	for (var i=0; i<str.length; i++) {
+		ofs++;
+
+		c = str.charAt(i);
+
+		if (needspace) { // continues <<<
+			if (is_space(c)) {
+				needspace = 0;
+				continue;
+			}
+			throw 'Garbage after list element at offset '+ofs+': "'+c+'"';
+		}
+		//>>>
+		if (!in_elem) { // fallthrough if c not a space <<<
+			if (is_space(c)) continue;
+			in_elem = true;
+			elemstart = true;
+		}
+		//>>>
+		if (elemstart) { // continues <<<
+			if (c == '{') {
+				braced = true;
+				bracedepth = true;
+				braceofs = ofs;
+			} else if (c == '"') {
+				quoted = true;
+			} else if (c == '\\') {
+				escaped = true;
+			} else {
+				elem += c;
+			}
+			elemstart = false;
+			continue
+		}
+		//>>>
+		if (escaped) { // sometimes falls through <<<
+			if (escape_mode == '') { //<<<
+				switch (c) {
+					case 'a':
+						elem += '\u0007';
+						escaped = false;
+						break;
+					case 'b':
+						elem += '\u0008';
+						escaped = false;
+						break;
+					case 'f':
+						elem += '\u000c';
+						escaped = false;
+						break;
+					case 'n':
+						elem += '\u000a';
+						escaped = false;
+						break;
+					case 'r':
+						elem += '\u000d';
+						escaped = false;
+						break;
+					case 't':
+						elem += '\u0009';
+						escaped = false;
+						break;
+					case 'v':
+						elem += '\u000b';
+						escaped = false;
+						break;
+
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+						escape_mode = 'octal';
+						escape_seq += c;
+						break;
+	
+					case 'x':
+						escape_mode = 'hex';
+						break;
+
+					case 'u':
+						escape_mode = 'unicode';
+						break;
+
+					default:
+						elem += c;
+						escaped = false;
+						break;
+				}
+				if (!escaped) {
+					escape_mode = '';
+				}
+				continue;
+				//>>>
+			} else if (escape_mode == 'octal') { //<<<
+				finished = false;
+				cont = false;
+				switch (c) {
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+						escape_seq += c;
+						if (escape_seq.length == 3) {
+							finished = true;
+						} else {
+							finished = false;
+						}
+						cont = true;
+						break;
+
+					default:
+						finished = true;
+						cont = false;
+						break;
+				}
+				if (finished) {
+					acc = 0;
+					pow = 0;
+					while (escape_seq.length > 0) {
+						var lsd = escape_seq.substr(-1,1);
+						escape_seq = escape_seq.slice(0,-1);
+						acc += lsd * Math.pow(8, pow);
+						pow++;
+					}
+					elem += unicode_char(acc);
+					escape_mode = '';
+					escaped = false;
+				}
+				if (cont) continue;
+				//>>>
+			} else if (escape_mode == 'hex') { //<<<
+				if (char_is_hex(c)) {
+					escape_seq += c;
+					continue;
+				} else {
+					if (escape_seq.length == 0) {
+						elem += 'x'+c;
+						escaped = false;
+						escape_mode = '';
+						continue;
+					}
+					if (escape_seq.length > 2) {
+						escape_seq = escape_seq.substr(-2, 2);
+					}
+				}
+				elem += unicode_char('0x'+escape_seq);
+				escape_mode = '';
+				escaped = false;
+				//>>>
+			} else if (escape_mode == 'unicode') { //<<<
+				finished = false;
+				cont = false;
+
+				if (char_is_hex(c)) {
+					escape_seq += c;
+					if (escape_seq.length == 4) {
+						finished = true;
+					} else {
+						finished = false;
+					}
+					cont = true;
+				} else {
+					finished = true;
+					cont = false;
+				}
+
+				if (finished) {
+					if (escape_seq.length == 0) {
+						elem += 'u';
+					} else {
+						while (escape_seq.length < 4) {
+							escape_seq = '0'+escape_seq;
+						}
+						elem += eval('"\\u'+escape_seq+'"');
+						escape_seq = '';
+					}
+					escape_mode = '';
+					escaped = false;
+				}
+
+				if (cont) continue
+				//>>>
+			} else {
+				throw 'Error in escape sequence parser state: invalid state "'+escape_mode+'"';
+			}
+		}
+		//>>>
+		if (braced) { // continues <<<
+			if (braceescape) {
+				elem += c;
+				braceescape = false;
+				continue;
+			}
+			if (c == '{') {
+				elem += c;
+				bracedepth++;
+			} else if (c == '}') {
+				bracedepth--;
+				if (bracedepth == 0) {
+					braced = false;
+					needspace = true;
+					in_elem = false;
+					parts.push(elem);
+					elem = '';
+				} else {
+					elem += c;
+				}
+			} else if (c == '\\') {
+				braceescape = true;
+			} else {
+				elem += c;
+			}
+			continue;
+		}
+		//>>>
+		if (quoted) { // continues <<<
+			if (c == '"') {
+				quoted = false;
+				in_elem = false;
+				parts.push(elem);
+				elem = '';
+				needspace = false;
+			} else if (c == '\\') {
+				escaped = true;
+			} else {
+				elem += c;
+			}
+			continue;
+		}
+		//>>>
+		if (is_space(c)) { // continues <<<
+			parts.push(elem);
+			elem = '';
+			in_elem = false;
+			continue;
+		}
+		//>>>
+		if (c == '\\') { // continues <<<
+			escaped = true;
+			continue;
+		}
+		//>>>
+
+		elem += c;
+	}
+
+	if (braced) { //<<<
+		throw 'Open brace in string (from offset '+braceofs+')';
+	}
+	//>>>
+	if (quoted) { //<<<
+		throw 'Open quote in string (from offset '+quoteofs+')';
+	}
+	//>>>
+	if (escaped) { //<<<
+		switch (escape_mode) {
+			case '':
+				elem += '\\';
+				parts.push(elem);
+				in_elem = false;
+				break;
+
+			case 'octal':
+				acc = 0;
+				pow = 0;
+				while (escape_seq.length > 0) {
+					var lsd = escape_seq.substr(-1,1);
+					escape_seq = escape_seq.slice(0,-1);
+					acc += lsd * Math.pow(8, pow);
+					pow++;
+				}
+				elem += unicode_char(acc);
+				escape_mode = '';
+				escaped = false;
+				break;
+
+			case 'hex':
+				if (escape_seq.length == 0) {
+					elem += 'x';
+				} else {
+					if (escape_seq.length > 2) {
+						escape_seq = escape_seq.substr(-2, 2);
+					}
+					elem += unicode_char('0x'+escape_seq);
+				}
+				escape_mode = '';
+				escaped = false;
+				break;
+
+			case 'unicode':
+				if (escape_seq.length == 0) {
+					elem += 'u';
+				} else {
+					while (escape_seq.length < 4) {
+						escape_seq = '0'+escape_seq;
+					}
+					elem += eval('"\\u'+escape_seq+'"');
+				}
+				escape_mode = '';
+				escaped = false;
+				break;
+
+			default:
+				throw 'Error in escape sequence parser state: invalid state "'+escape_mode+'"';
+				break;
+		}
+	}
+	//>>>
+	if (in_elem) { //<<<
+		parts.push(elem);
+		elem = '';
+	}
+	//>>>
+
+	return parts;
+}
+
+//>>>
+function serialize_tcl_list(arr) { //<<<
+	// for now...
+	var i, staged, elem;
+	staged = [];
+	for (i=0; i<arr.length; i++) {
+		elem = String(arr[i]);
+		if (
+			elem.length > 0 &&
+			elem.indexOf(' ') == -1 &&
+			elem.indexOf('"') == -1 &&
+			elem.indexOf('}') == -1 &&
+			elem.indexOf('{') == -1 &&
+			elem.indexOf('\t') == -1 &&
+			elem.indexOf('\n') == -1 &&
+			elem.indexOf('\r') == -1 &&
+			elem.indexOf('\v') == -1
+		) {
+			if (elem.indexOf('\\') == -1) {
+				staged.push(elem);
+			} else {
+				// Replace all \ with \\
+				staged.push(elem.replace(/\\/g, '\\$&'));	// WARNING: flags are a spidermonkey extension
+			}
+		} else {
+			if (
+				elem.indexOf('}') == -1 &&
+				elem.indexOf('{') == -1 &&
+				elem.charAt(elem.length-1) != '\\'
+			) {
+				staged.push('{'+elem+'}');
+			} else {
+				// Replace all <special> with \<special>
+				staged.push(elem.replace(/\\| |"|}|{|\t|\n|\r|\v/g, '\\$&'));	// WARNING: flags are a spidermonkey extension
+			}
+		}
+	}
+	return staged.join(' ');
+}
+
+//>>>
+function list2dict(list) { //<<<
+	return array2dict(parse_tcl_list(list));
+}
+
+//>>>
+function array2dict(arr) { //<<<
+	var build, i;
+	build = {};
+
+	for (i=0; i<arr.length; i+=2) {
+		var k, v;
+		k = arr[i];
+		v = arr[i+1];
+
+		build[k] = v;
+	}
+
+	return build;
+}
+
+//>>>
